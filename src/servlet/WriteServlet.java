c3667d7b68fe86aae0d03b2b66ae118c2c2fc125
Kim-Ju-Yeop <kjy031104@gmail.com>
2020-07-09 13:26:40 +0900

    [ADD] Write.html

--- src/servlet/WriteServlet.java
+++ src/servlet/WriteServlet.java
@@ -5,6 +5,7 @@ import model.BulletinModel;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,6 +14,17 @@ import java.io.IOException;
 @WebServlet(name = "WriteServlet", urlPatterns = "/writeServlet")
 public class WriteServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String cookieId = null;
+        Cookie[] cookies = request.getCookies();
+
+        for(Cookie cookie : cookies){
+            if ("sessionId".equals(cookie.getName())) {
+                cookieId = cookie.getValue();
+                break;
+            }
+        }
+
         try{
             SimpleDbBulletin simpleDbBulletin = new SimpleDbBulletin();
 
@@ -20,7 +32,7 @@ public class WriteServlet extends HttpServlet {
             int hit = 0;
             String title = request.getParameter("title");
             String content = request.getParameter("content");
-            String writer = request.getParameter("writer");
+            String writer = cookieId;
 
             BulletinModel model = new BulletinModel(id, hit, title, content, writer);
             simpleDbBulletin.writeList(model);
@@ -31,6 +43,21 @@ public class WriteServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String cookieId = null;
+        Cookie[] cookies = request.getCookies();
 
+        for (Cookie cookie : cookies) {
+            if ("sessionId".equals(cookie.getName())) {
+                cookieId = cookie.getValue();
+                break;
+            }
+        }
+
+        if(cookieId == null){
+            response.setCharacterEncoding("charset=UTF-8");
+            response.getWriter().print(0);
+        }else{
+            response.getWriter().print(1);
+        }
     }
 }
